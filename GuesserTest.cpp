a4d351c749481ee9a1236ab7ebc9594067a47de3
Pano Klironomos <panoklironomos@gmail.com>
2026-04-29 11:44:28 -0700

    Added tests

--- GuesserTest.cpp
+++ GuesserTest.cpp
@@ -14,9 +14,55 @@ class GuesserTest : public ::testing::Test
 		virtual void TearDown(){} //clean up after each test, (before destructor)
 };
 
-// Example "smoke test" (can be deleted)
-TEST(GuesserTest, smoke_test)
+
+TEST(GuesserTest, distance_test)
 {
-  Guesser object("Secret");
-  ASSERT_EQ( 1+1, 2 );
+	// normal secret
+	Guesser object("Secret");
+	ASSERT_EQ(object.distance("Secret") , 0);
+	ASSERT_EQ(object.distance("Secre") , 1);
+	ASSERT_EQ(object.distance("SecreT") , 1);
+	ASSERT_EQ(object.distance("Secrete") , 1);
+	ASSERT_EQ(object.distance("xxcret") , 2);
+	ASSERT_EQ(object.distance("1e2r3t") , 3);
+	ASSERT_EQ(object.distance("Sxxxxt") , 4);
+	ASSERT_EQ(object.distance("SECRET") , 5);
+	ASSERT_EQ(object.distance("_Secret_") , 6);
+	ASSERT_EQ(object.distance("") , 6);
+	ASSERT_EQ(object.distance("ABCDEFGHIJKLMNOP") , 6);
+
+	// empty secret
+	Guesser object2("");
+	ASSERT_EQ(object2.distance("") , 0);
+	ASSERT_EQ(object2.distance("Secret") , 0);
+	ASSERT_EQ(object2.distance("Secre") , 0);
+	ASSERT_EQ(object2.distance("__cret") , 0);
+	ASSERT_EQ(object2.distance("SECRET") , 0);
+	ASSERT_EQ(object2.distance("ABCDEFGHIJKLMNOP") , 0);
+}
+
+TEST(GuesserTest, match_test)
+{
+	// Guessed right
+	Guesser object("Secret");
+	ASSERT_EQ(object.match("Secre") , false);
+	ASSERT_EQ(object.match("Secrete") , false);
+	ASSERT_EQ(object.match("Secret") , true);
+
+	// Guessed wrong 3 times
+	Guesser object2("Secret");
+	ASSERT_EQ(object2.match("Secre") , false);
+	ASSERT_EQ(object2.match("Secrete") , false);
+	ASSERT_EQ(object2.match("SecreT") , false);
+	ASSERT_EQ(object2.match("Secret") , false); // Always false after 3 wrong guesses
+
+	// Guessed wrong by dist > 2
+	Guesser object3("Secret");
+	ASSERT_EQ(object3.match("___ret") , false);
+	ASSERT_EQ(object3.match("Secret") , false); // Always false after dist > 2
+
+	//short case, dist can never be > 2
+	Guesser object4("ab");
+	ASSERT_EQ(object4.match("123456") , false);
+	ASSERT_EQ(object4.match("ab") , true);
 }
